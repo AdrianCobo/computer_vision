5966c5f840f6b53631a0d4a5f190365f0471b471
adrianCo <adrian.cobo.mh@gmail.com>
2025-02-14 11:42:49 +0100

    works for 160 grad
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- include/computer_vision/PlcSync.hpp
+++ include/computer_vision/PlcSync.hpp
@@ -249,21 +249,20 @@ private:
       final_pcl.reserve(image_depth_ptr1->image.rows * image_depth_ptr1->image.cols * N_CAMS);
       
       depth2pcl(image_depth_ptr1->image, camera_model1_, final_pcl);
-
-      // borrar despues de prueba
-      // depth2pcl(image_depth_ptr2->image, camera_model2_, final_pcl);
-      // depth2pcl(image_depth_ptr3->image, camera_model3_, final_pcl);
-
       
       depth2pcl(image_depth_ptr2->image, camera_model2_, temp_pcl);
       Eigen::Vector3f translation(-0.09698, 0.0, -0.01710);
-      y_rotation(temp_pcl, final_pcl, translation, -30*M_PI/180);
+      Eigen::Vector3f translation2(0.0, 0.0, 0);
+      y_rotation(temp_pcl, temp_pcl, translation, 0.0);
+      y_rotation(temp_pcl, final_pcl, translation2, -30*M_PI/180);
+
       temp_pcl.clear();
 
 
       depth2pcl(image_depth_ptr3->image, camera_model3_, temp_pcl);
       translation = Eigen::Vector3f(0.09698, 0.0, -0.01710);
-      y_rotation(temp_pcl, final_pcl, translation, -330*M_PI/180);
+      y_rotation(temp_pcl, temp_pcl, translation, 0.0);
+      y_rotation(temp_pcl, final_pcl, translation2, -330*M_PI/180);
 
       sensor_msgs::msg::PointCloud2 out_pointcloud;
       pcl::toROSMsg(final_pcl, out_pointcloud);
